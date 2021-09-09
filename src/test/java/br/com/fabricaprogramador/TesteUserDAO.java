bafc9539aa6ad2269fb3d6f1e66459db5371de1d
vinidesenvolve <vinidesenvolve@gmail.com>
2021-09-09 14:38:06 -0300

    Organizando arquivo DAO

--- src/test/java/br/com/fabricaprogramador/TesteUserDAO.java
+++ src/test/java/br/com/fabricaprogramador/TesteUserDAO.java
@@ -7,5 +7,9 @@ public class TesteUserDAO {
     public static void main(String[] args){
 
         User user = new User();
+        user.setName("Antonio Vivaldi");
+        user.setLogin("vivaldi");
+        user.setPassword("1234");
+        
     }
 }
