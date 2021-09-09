f8db64b8670b5aae532e5e84f4b1e7acaeb41dc0
vinidesenvolve <vinidesenvolve@gmail.com>
2021-09-09 14:31:03 -0300

    Criaçao classe de teste para User

---
+++ src/test/java/br/com/fabricaprogramador/TesteUserDAO.java
@@ -0,0 +1,11 @@
+package br.com.fabricaprogramador;
+
+import br.com.fabricaprogramador.entity.User;
+
+public class TesteUserDAO {
+    
+    public static void main(String[] args){
+
+        User user = new User();
+    }
+}
