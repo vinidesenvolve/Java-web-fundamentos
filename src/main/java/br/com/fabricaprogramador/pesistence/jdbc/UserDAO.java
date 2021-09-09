1cd725b39a5d324e5481b630956af4ba47d36abc
vinidesenvolve <vinidesenvolve@gmail.com>
2021-09-09 17:42:24 -0300

    Implementaçao buscar de usuario

--- src/main/java/br/com/fabricaprogramador/pesistence/jdbc/UserDAO.java
+++ src/main/java/br/com/fabricaprogramador/pesistence/jdbc/UserDAO.java
@@ -1,92 +1,119 @@
 package br.com.fabricaprogramador.pesistence.jdbc;
 
+import br.com.fabricaprogramador.pesistence.entity.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import br.com.fabricaprogramador.pesistence.entity.User;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDAO {
-    private Connection con = ConnectionFactory.getConnection();
-    
-    public void save(User user) {
-        String sql = "insert into users (name, login, password) values (?,?,?)";
 
-        try {
-            PreparedStatement statement = con.prepareStatement(sql);
+  private Connection con = ConnectionFactory.getConnection();
 
-            statement.setString(1, user.getName());
-            statement.setString(2, user.getLogin());
-            statement.setString(3, user.getPassword());
+  public void save(User user) {
+    String sql = "insert into users (name, login, password) values (?,?,?)";
 
-            statement.execute();
-            statement.close();
+    try {
+      PreparedStatement statement = con.prepareStatement(sql);
 
-        } catch (SQLException e) {
-              e.printStackTrace();
-        }
+      statement.setString(1, user.getName());
+      statement.setString(2, user.getLogin());
+      statement.setString(3, user.getPassword());
+
+      statement.execute();
+      statement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void update(User user) {
+    String sql = "update users set name=?, login=?, password=? where id=?";
+
+    try {
+      PreparedStatement statement = con.prepareStatement(sql);
+
+      statement.setString(1, user.getName());
+      statement.setString(2, user.getLogin());
+      statement.setString(3, user.getPassword());
+      statement.setInt(4, user.getId());
+
+      statement.execute();
+      statement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void delete(Integer id) {
+    String sql = "delete from users where id=?";
+
+    try {
+      PreparedStatement statement = con.prepareStatement(sql);
+
+      statement.setInt(1, id);
+
+      statement.execute();
+      statement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public User findById(Integer id) {
+    String sql = "select * from users where id=?";
+
+    try {
+      PreparedStatement statement = con.prepareStatement(sql);
+
+      statement.setInt(1, id);
+      ResultSet result = statement.executeQuery();
+
+      if (result.next()) {
+        User user = new User();
+        user.setId(result.getInt("id"));
+        user.setName(result.getString("name"));
+        user.setLogin(result.getString("login"));
+        user.setPassword(result.getString("password"));
+
+        return user;
+      }
+
+      statement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    public void update(User user) {
-        String sql = "update users set name=?, login=?, password=? where id=?";
+    return null;
+  }
 
-        try {
-            PreparedStatement statement = con.prepareStatement(sql);
+  public List<User> findAll() {
+    String sql = "select * from users";
+    List<User> users = new ArrayList<>();
 
-            statement.setString(1, user.getName());
-            statement.setString(2, user.getLogin());
-            statement.setString(3, user.getPassword());
-            statement.setInt(4, user.getId());
+    try {
+      PreparedStatement statement = con.prepareStatement(sql);
 
-            statement.execute();
-            statement.close();
+      ResultSet result = statement.executeQuery();
 
-        } catch (SQLException e) {
-              e.printStackTrace();
-        }
+      while (result.next()) {
+        User user = new User();
+        
+        user.setId(result.getInt("id"));
+        user.setName(result.getString("name"));
+        user.setLogin(result.getString("login"));
+        user.setPassword(result.getString("password"));
+
+        users.add(user);
+      }
+
+      statement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    public void delete(Integer id) {
-        String sql = "delete from users where id=?";
-
-        try {
-            PreparedStatement statement = con.prepareStatement(sql);
-
-            statement.setInt(1, id);
-
-            statement.execute();
-            statement.close();
-
-        } catch (SQLException e) {
-              e.printStackTrace();
-        }
-    }
-
-    public User findById(Integer id) {
-        String sql = "select * from users where id=?";
-
-        try {
-            PreparedStatement statement = con.prepareStatement(sql);
-
-            statement.setInt(1, id);
-            ResultSet result = statement.executeQuery();
-
-            if(result.next()) {
-                User user = new User();
-                user.setId(result.getInt("id"));
-                user.setName(result.getString("name"));
-                user.setLogin(result.getString("login"));
-                user.setPassword(result.getString("password"));
-
-                return user;
-            }
-
-            statement.close();
-        } catch (SQLException e) {
-              e.printStackTrace();
-        }
-
-        return null;
-    }
+    return users;
+  }
 }
--- src/test/java/br/com/fabricaprogramador/TesteUserDAO.java
+++ src/test/java/br/com/fabricaprogramador/TesteUserDAO.java
@@ -2,18 +2,19 @@ package br.com.fabricaprogramador;
 
 import br.com.fabricaprogramador.pesistence.entity.User;
 import br.com.fabricaprogramador.pesistence.jdbc.UserDAO;
+import java.util.List;
 
 public class TesteUserDAO {
 
   public static void main(String[] args) {
-      saveUser();
+    findUsers();
   }
 
   private static void saveUser() {
     User user = new User();
-    
-    user.setName("Antonio Vivaldi");
-    user.setLogin("vivaldi");
+
+    user.setName("Mozart");
+    user.setLogin("moza");
     user.setPassword("1234");
 
     UserDAO userDAO = new UserDAO();
@@ -22,7 +23,7 @@ public class TesteUserDAO {
 
   private static void updateUser() {
     User user = new User();
-    
+
     user.setId(1);
     user.setName("Antônio Vivaldi");
     user.setLogin("vival");
@@ -33,8 +34,21 @@ public class TesteUserDAO {
   }
 
   private static void deleteUser() {
-      UserDAO userDAO = new UserDAO();
-      userDAO.delete(1);
+    UserDAO userDAO = new UserDAO();
+    userDAO.delete(1);
   }
- 
+
+  private static void findUserById() {
+    UserDAO userDAO = new UserDAO();
+    User user = userDAO.findById(2);
+
+    System.out.println(user.getName());
+  }
+
+  private static void findUsers() {
+    UserDAO userDAO = new UserDAO();
+    List<User> users = userDAO.findAll();
+
+    users.stream().forEach(user -> System.out.println(user.getName()));
+  }
 }
